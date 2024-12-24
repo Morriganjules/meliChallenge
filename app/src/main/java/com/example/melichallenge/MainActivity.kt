62f80847c6a4ca031c68d4b0a6810d5fcaddf79d
Jules <jules.ferreira@naranjax.com>
2024-12-24 14:16:21 -0300

    dumb commit

--- app/src/main/java/com/example/melichallenge/MainActivity.kt
+++ app/src/main/java/com/example/melichallenge/MainActivity.kt
@@ -9,13 +9,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -36,7 +34,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MeliChallengeTheme {
                 val navController = rememberNavController()
-
+                //prueba commit
                 NavHost(
                     navController = navController,
                     startDestination = "splash"
